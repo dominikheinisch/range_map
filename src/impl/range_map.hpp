6caac15f51d03afd2a47cad40a1694f8ce671768
dominik <dominikheinisch2@gmail.com>
2020-03-28 18:25:38 +0100

    added skeleton of range_map

---
+++ src/impl/range_map.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <map>
+#include <limits>
+#include <utility>
+
+
+template<typename K, typename V>
+class range_map {
+
+private:
+    std::map<K, V> m_map;
+
+public:
+    range_map(const V& val) {
+        m_map.emplace_hint(m_map.end(), std::numeric_limits<K>::lowest(), val);
+    }
+
+    void add(const K& keyBegin, const K& keyEnd, const V& val ) {
+       // TODO
+    }
+
+    const V& operator[](const K& key ) const {
+        return (--m_map.upper_bound(key))->second;
+    }
+};
